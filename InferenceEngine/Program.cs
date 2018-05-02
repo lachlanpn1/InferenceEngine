fa7423a18cfcfb9398536ff592294f12fe81433a
jGibbs <joe.gibbs0@gmail.com>
2018-05-02 11:28:02 +1000

    Commit 1: added code

--- InferenceEngine/Program.cs
+++ InferenceEngine/Program.cs
@@ -10,6 +10,18 @@ namespace InferenceEngine
     {
         static void Main(string[] args)
         {
+            if (args.Length == 2)
+            {
+                if (!((args[0] == "TT") || (args[0] == "FC") || (args[0] == "BC")))
+                {
+                    Console.WriteLine("Error: The second argument must be either TT, FC or BC");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Error: There must be 3 command line arguments: the path to ");
+            }
         }
     }
 }
