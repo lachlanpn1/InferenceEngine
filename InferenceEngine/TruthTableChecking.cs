43e54077610345786d6887b12b8229b8753acd86
jGibbs <joe.gibbs0@gmail.com>
2018-05-04 14:45:10 +1000

    Added code to TTC

--- InferenceEngine/TruthTableChecking.cs
+++ InferenceEngine/TruthTableChecking.cs
@@ -15,14 +15,14 @@ namespace InferenceEngine
 
         private bool Entails(List<Sentence> kb, Sentence a)
         {
-            List<String> symbols = new List<string>();
-
-
+            List<String> symbols = KnowledgeBase.Symbols;
+            symbols.AddRange(a.GetSymbols());
             return CheckAll(kb, a, symbols, model);
         }
 
-        private bool CheckAll(List<Sentence> kb, Sentence a, List<String> symbols, )
+        private bool CheckAll(List<Sentence> kb, Sentence a, List<SimpleSentence> symbols, List<Sentence> model)
         {
+            Sentence p;
             if (symbols.Count < 1)
             {
                 if (true)
@@ -38,8 +38,13 @@ namespace InferenceEngine
 
             else
             {
-
+                p = symbols.First();
             }
+        }
+
+        private bool PLTRUE()
+        {
+
         }
     }
 }
