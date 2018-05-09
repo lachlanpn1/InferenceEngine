757c9bdef88ff3c82794424c22ad3133101b4962
Lachlan <101626915@student.swin.edu.au>
2018-05-09 13:37:38 +1000

    Added Model.cs

---
+++ InferenceEngine/Model.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InferenceEngine
+{
+    class Model
+    {
+        Dictionary<string, bool> _model = new Dictionary<string, bool>();
+        public Model()
+        {
+
+        }
+
+        public Dictionary<string, bool> getModel
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                _model = value;
+            }
+        }
+
+        public Model Extend(string proposition, bool value)
+        {
+            Model temp = this;
+            temp.getModel.Add(proposition, value);
+            return temp;
+        }
+    }
+}
