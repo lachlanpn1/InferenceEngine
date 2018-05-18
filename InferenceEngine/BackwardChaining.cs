3a2f573e103125476a0f6dc1b6e3030c3a98611d
jGibbs <joe.gibbs0@gmail.com>
2018-05-18 13:25:45 +1000

    added code to backward chaining

--- InferenceEngine/BackwardChaining.cs
+++ InferenceEngine/BackwardChaining.cs
@@ -12,5 +12,24 @@ namespace InferenceEngine
         {
             throw new NotImplementedException();
         }
+
+        private List<string> FOL_BC_ASK(KnowledgeBase KB, List<Sentence> goals, Sentence θ)
+        {
+            List<String> answers = new List<string>();
+            if (goals.Count < 1)
+            {
+                return θ;
+            }
+            Sentence q = ;
+            foreach (Sentence r in KB.KB)
+            {
+                if ((standardize-apart(r)) == (unify(q, !q))
+                {
+                    new_goals = ;
+                    answers = 
+                }
+            }
+            return answers;
+        }
     }
 }
